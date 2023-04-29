a9d1c6d36e8724bc4d47918f31d767068488e444
Nick Winner <design@winnerdigital.net>
2023-04-29 14:44:37 -0400

    :coffin: Remove unused validation code

--- src/Command.ts
+++ src/Command.ts
@@ -16,35 +16,6 @@ export abstract class Command {
     }
 
     return this._validate();
-
-    // const nameLower = this.name.toLowerCase();
-
-    // switch (nameLower) {
-    //   case VALID_COMMANDS.LIST:
-    //     return { valid: true };
-    //   case VALID_COMMANDS.CREATE:
-    //   case VALID_COMMANDS.DELETE:
-    //     return this.args.length >= 1
-    //       ? { valid: true }
-    //       : {
-    //           valid: false,
-    //           message: `CommandValidationError: Please provide a path at which to ${nameLower} the item.`,
-    //         };
-    //   case VALID_COMMANDS.MOVE:
-    //     return this.args.length >= 2
-    //       ? { valid: true }
-    //       : {
-    //           valid: false,
-    //           message:
-    //             'CommandValidationError: Please provide a target and destination path.',
-    //         };
-
-    //   default:
-    //     return {
-    //       valid: false,
-    //       message: `CommandValidationError: Unknown command '${this.name}'.`,
-    //     };
-    // }
   }
 
   protected abstract _validate(): ValidatorResult;
