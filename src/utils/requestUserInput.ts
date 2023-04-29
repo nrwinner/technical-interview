163d24d142e0f3285f7cf14ba934c4e28498c825
Nick Winner <design@winnerdigital.net>
2023-04-29 10:15:21 -0400

    :fire: Remove timeout on user input

--- src/utils/requestUserInput.ts
+++ src/utils/requestUserInput.ts
@@ -1,23 +1,15 @@
 import { createInterface } from 'readline';
 
-const ONE_MINUTE = 60000;
-
 export const requestUserInput: (prompt: string) => Promise<string> = (
   prompt: string
 ) => {
-  return new Promise((resolve, reject) => {
+  return new Promise((resolve) => {
     const readlineInterface = createInterface({
       input: process.stdin,
       output: process.stdout,
     });
 
-    const requestTimeout = setTimeout(() => {
-      readlineInterface.close();
-      reject('Request for user input timed out.');
-    }, ONE_MINUTE);
-
     readlineInterface.question(prompt, (response: string) => {
-      clearTimeout(requestTimeout);
       readlineInterface.close();
       resolve(response);
     });
